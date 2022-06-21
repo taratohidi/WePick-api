efc4a28e5f0513ea16643fdf5a8de8f4543c1f91
Tara Tohidi <tara.tohidi@gmail.com>
2022-06-21 15:39:34 -0700

    Initial setup of index file and create routes folder and files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { PORT, BACKEND_URL } = process.env;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json()); // allows posting req.body
+
+app.use(express.static("public")); // allows public folder to share
+
+// Welcome Page
+app.get("/", (req, res) => {
+  res.send("welcome to my API server");
+});
+
+// Check Api Server Status
+app.get("/status", (req, res) => {
+  res.send("Server is online");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running at ${BACKEND_URL}:${PORT}`);
+});
